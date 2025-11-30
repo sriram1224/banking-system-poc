c6526062998c4c20db9d6298a4904f6d877b5421
Bhargav Sri Ram <kasukurthibhargav@gmail.com>
2025-11-30 20:05:54 +0530

    Cors

--- system2-corebank/src/main/java/com/bank/poc/core/config/CorsConfig.java
+++ system2-corebank/src/main/java/com/bank/poc/core/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","banking-system-poc-omega.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
